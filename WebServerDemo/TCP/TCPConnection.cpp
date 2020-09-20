e87c00beeb3302d755ea47a152550b19478997ca
HamzehAmiri2020@yahoo.com <Sanaz13671367>
2020-09-20 23:47:43 +0330

    Socket

---
+++ WebServerDemo/TCP/TCPConnection.cpp
@@ -0,0 +1,9 @@
+#include <memory>
+#include <WinSock2.h>
+
+using namespace std;
+
+class TCPConnection : public enable_shared_from_this<TCPConnection> {
+protected:
+public:
+};
--- WebServerDemo/WebServerDemo.cpp
+++ WebServerDemo/WebServerDemo.cpp
@@ -1,20 +1,56 @@
-// WebServerDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
+#include <stdio.h>
+#include <WinSock2.h>
 #include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int serverSocket, clientSocket;
+	WSADATA wsa_data;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa_data)) {
+		WSACleanup();
+	}
+
+	serverSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSocket < 0)
+		std::cout << "Error";
+
+	std::string ip = "127.0.0.1";
+
+	struct sockaddr_in serv_addr, client_addr;
+	uint16_t port = 1256;
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+	serv_addr.sin_port = htons(port);
+
+	::bind(serverSocket, (SOCKADDR*)&serv_addr, sizeof(serv_addr));
+	::listen(serverSocket, 2);
+
+	while (true)
+	{
+		socklen_t lengh = sizeof client_addr;
+		clientSocket = ::accept(serverSocket, (SOCKADDR*)&client_addr, &lengh);
+
+		char event[10] = { 0 };
+		char messageToClient[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=ISO-8859-4 \r\n<h1>Hello, client! Welcome to the Virtual Machine Web..</h1> \r\n\r\n";
+
+		/*unsigned long on = 0;
+		ioctlsocket(clientSocket, FIONBIO, &on);*/
+
+		int on = 1;
+		setsockopt(clientSocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&on, sizeof on);
+
+		::send(clientSocket, messageToClient, strlen(messageToClient), 0);
+		while (recv(clientSocket, event, 10, 0) > 0) {
+			std::cout << event;
+		}
+		std::cout << "End";
+		closesocket(clientSocket);
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
